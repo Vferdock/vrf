7d7e72316e3247f155ca55cbf9af8f94c7e7c8b3
kyle1james <kyle1james@gmail.com>
2015-12-14 14:07:32 -0500

    wjenfivonwo

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get Home" do
+    get :Home
+    assert_response :success
+  end
+
+  test "should get Help" do
+    get :Help
+    assert_response :success
+  end
+
+  test "should get Articles" do
+    get :Articles
+    assert_response :success
+  end
+
+end
